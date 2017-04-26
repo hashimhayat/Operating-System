3eadb2b11fb1a2a93a01a009ea3afb845f188f1c
hashimhayat <hashimhayat@gmail.com>
2017-04-26 14:24:40 -0400

    update

--- 02_Scheduling/processhandler.py
+++ 02_Scheduling/processhandler.py
@@ -44,31 +44,12 @@ class ProcessTable:
 
 	def readFile(self,filePath):
 		fd = open(filePath,'r')
-		content = re.sub('[ \t]{2,}','-',fd.read().strip()).split('-')
+		#content = re.sub('[ \t]{2,}','-',fd.read().strip()).split('-')
+		content = re.sub('[ \t]{2,}','-',fd.read().strip()).replace(' (','-(').replace('(','').replace(')','').split('-')
+		print(content)
 		self.count = int(content[0])
 		self.buildStore(content,self.count)
-		
 		fd.close()
-
-	def clean(self, raw):
-		clean = []
-		clean.append(raw[0])
-		raw = raw[1:].split()
-
-		c = 0
-		tmp = ''
-		for n in raw:
-			if n.isdigit(): 
-				tmp += n + ' '
-				c += 1
-				if c == 4:
-					tmp = tmp[:-1]
-					clean.append(tmp)
-					tmp = ''
-					c = 0
-
-		print(clean)
-		return clean
 
 	# Parse the raw data and returns process objects
 
